983259112cb9bccabea22ccb232ae22049e2e123
Liangjun Zheng <zhengliangjun@outlook.com>
2026-06-21 10:44:42 +0800

    fix: fix WebSocket handler tests that mock sealed classes

--- tests/FlowEngine.Host.Tests/ExecutionWebSocketHandlerTests.cs
+++ tests/FlowEngine.Host.Tests/ExecutionWebSocketHandlerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Moq;
 using Microsoft.Extensions.Logging;
+using System.Net.WebSockets;
 
 namespace FlowEngine.Host.Tests;
 
